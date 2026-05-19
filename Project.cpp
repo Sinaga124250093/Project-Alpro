65ad6afc7bbed5c828b59c3884044e0ea94853f7
Alwan Arkan <alwantamirtask@gmail.com>
2026-05-19 22:23:48 +0700

    Add struct aspirasi & beasiswa

--- Project.cpp
+++ Project.cpp
@@ -1,6 +1,29 @@
-
-
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+struct Aspirasi {
+    char nim[20];
+    char keluhan[255];
+    Aspirasi* next;
+};
+Aspirasi* headAspirasi = NULL;
+
+struct Beasiswa {
+    char nim[20];
+    char nama[50];
+    float ipk;
+    int urgensi;
+    Beasiswa* next;
+};
+Beasiswa* headBeasiswa = NULL;
+Beasiswa* tailBeasiswa = NULL;
+
+
+
+
+
 
 int main(int argc, char **argv)
 {
