0ca09cad30bf05fadb211c83b72204d4e31bfcff
Juan Sinaga <alvinsinaga048@gmail.com>
2026-05-20 18:37:40 +0700

    Add Void Aspirasi

--- Project.cpp
+++ Project.cpp
@@ -19,7 +19,37 @@ struct Beasiswa {
 };
 Beasiswa* headBeasiswa = NULL;
 Beasiswa* tailBeasiswa = NULL;
+void tambahAspirasi() {
+    Aspirasi* nodeBaru = new Aspirasi;
+    cout << "\n--- Tambah Aspirasi / Keluhan ---" << endl;
+    cout << "Masukkan NIM: "; cin >> nodeBaru->nim;
+    cout << "Keluhan (Gunakan '_' untuk spasi): "; cin >> nodeBaru->keluhan;
+    
+    nodeBaru->next = headAspirasi;
+    headAspirasi = nodeBaru;
+    
+    cout << "Data aspirasi berhasil ditambahkan di awal antrean!" << endl;
+}
 
+void tambahBeasiswa() {
+    Beasiswa* nodeBaru = new Beasiswa;
+    cout << "\n--- Pengajuan Beasiswa ---" << endl;
+    cout << "Masukkan NIM: "; cin >> nodeBaru->nim;
+    cout << "Nama (Gunakan '_' untuk spasi): "; cin >> nodeBaru->nama;
+    cout << "IPK: "; cin >> nodeBaru->ipk;
+    cout << "Tingkat Urgensi (1-5): "; cin >> nodeBaru->urgensi;
+    
+    nodeBaru->next = NULL;
+    
+    if (headBeasiswa == NULL) {
+        headBeasiswa = nodeBaru;
+        tailBeasiswa = nodeBaru;
+    } else {
+        tailBeasiswa->next = nodeBaru;
+        tailBeasiswa = nodeBaru;
+    }
+    cout << "Data pengajuan beasiswa masuk ke dalam antrean (Tail)!" << endl;
+}
 
 
 
